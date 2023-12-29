b5f40d5e9d3f8edc60b85b6a197416f065c63c69
sagar <sagarc2580@gmail.com>
2023-12-29 16:09:51 +0530

    comments added

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-function saveToLocal(event) {
-  event.preventDefault();
-  const amount = event.target.amount.value;
-  const description = event.target.des.value;
-  const category = event.target.cat.value;
+let form = document.getElementById("myForm");
+let amount = document.getElementById("amount");
+let description = document.getElementById("description");
+let options = document.getElementById("options");
+let btn = document.getElementById("add");
+let list = document.getElementById("list");
 
-  const object = {
-    amount,
-    description,
-    category,
+form.addEventListener("submit", addExpenses);
+
+function addExpenses(e) {
+  e.preventDefault();
+
+  //adding the data to li
+  let li = document.createElement("li");
+  li.className = "expenses";
+  var userData = document.createTextNode(
+    `${amount.value}-${options.value}-${description.value}`
+  );
+  li.appendChild(userData);
+
+  //adding buttons
+  let dltBtn = document.createElement("button");
+  dltBtn.className = "deleteData";
+  dltBtn.appendChild(document.createTextNode("Delete Exepenses"));
+  li.appendChild(dltBtn);
+  let editBtn = document.createElement("button");
+  editBtn.className = "editData";
+  editBtn.appendChild(document.createTextNode("Edit Expenses"));
+  li.appendChild(editBtn);
+
+  //append li to ul
+  list.appendChild(li);
+
+  //Storing info in local storage making the description as unique key
+  let expenseAmount = amount.value;
+  let expenseCategory = options.value;
+  let expenseDescription = description.value;
+  let input = {
+    expenseAmount,
+    expenseCategory,
+    expenseDescription,
+  };
+  localStorage.setItem(input.expenseDescription, JSON.stringify(input));
+
+  //to delete data from screen and local storage
+  var parentEle = document.getElementById("list");
+  dltBtn.onclick = () => {
+    localStorage.removeItem(input.expenseDescription);
+    parentEle.removeChild(li);
   };
 
-  localStorage.setItem(object.category, JSON.stringify(object));
-  show(object);
-}
+  //to edit data
+  var parentEle = document.getElementById("list");
+  editBtn.onclick = () => {
+    amount.value = input.expenseAmount;
+    description.value = input.expenseDescription;
+    options.value = input.expenseCategory;
+    localStorage.removeItem(input.expenseDescription);
+    parentEle.removeChild(li);
+  };
 
-window.addEventListener("DOMContentLoaded", () => {
-  const localStoreObjects = localStorage;
-  const localStoreKeys = Object.keys(localStoreObjects);
-
-  // loop through those keys
-  for (let i = 0; i < localStoreKeys.length; i++) {
-    const key = localStoreKeys[i];
-    const detailString = localStoreObjects[key];
-    const detailObj = JSON.parse(detailString);
-    show(detailObj);
-  }
-});
-
-// showing whatever we got in UI
-function show(details) {
-  const parent = document.getElementById("listhere");
-
-  const child = `<li id=${details.category}> ${details.amount} - ${details.description}
-    <button onclick=deleteUser('${details.category}')> Delete User </button>
-  </li>`;
-  parent.innerHTML += child;
-}
-
-function deleteUser(category) {
-  localStorage.removeItem(category);
-  removeFromScreen(category);
-}
-
-function removeFromScreen(category) {
-  const parent = document.getElementById("listhere");
-  const child = document.getElementById(category);
-  if (child) {
-    parent.removeChild(child);
-  }
+  //to empty the fields again after data has been submitted
+  amount.value = "";
+  description.value = "";
 }
